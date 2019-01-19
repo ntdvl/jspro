17f49a3ab2ada763fbc93f2d3abe100b09440d1c
ntdvl <yilmazkoman@outlook.com>
2019-01-19 11:35:51 +0300

    Added 1. Gun

---
+++ 1. Gun/plaque/app.js
@@ -0,0 +1,7 @@
+function fevent(x) {
+
+   var y = x.value.length;
+
+   var p = document.getElementById('cost').innerHTML = (y * 5) + ' $ ';
+
+}
---
+++ 1. Gun/script/app.js
@@ -0,0 +1,8 @@
+function checkRoom() {
+
+    alert('checked');
+
+}
+
+
+checkRoom();
