440ce1fd6e19d4858f4b93c4c469124b1f56a10d
ntdvl <yilmazkoman@outlook.com>
2019-05-04 02:09:00 +0300

    Added 20. Gun

---
+++ 20. Gun/1. Calisma/app.js
@@ -0,0 +1,5 @@
+(function load() {
+
+    document.write('lorem');
+
+})();
---
+++ 20. Gun/2. Calisma/app.js
@@ -0,0 +1,6 @@
+(function load() {
+
+    var p = document.getElementsByTagName('p')[0];
+    document.write(p.innerHTML);
+
+})();
